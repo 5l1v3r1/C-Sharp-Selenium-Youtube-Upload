8af2b067d499976987376978315c7141bdf8214e
Osman Yavuz <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-03-12 09:52:50 +0300

    Update

---
+++ YoutubeUpload/YoutubeUpload/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace YoutubeUpload
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Giriş Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Tarayıcı Kapat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(134, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(654, 118);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(453, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ YoutubeUpload/YoutubeUpload/Form1.cs
@@ -0,0 +1,159 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeUpload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Driver
+        public ChromeDriver Chrome_Sekme;
+        private ChromeDriverService Chrome_Servis = ChromeDriverService.CreateDefaultService();
+        private ChromeOptions Chrome_Ayar = new ChromeOptions();
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            // Driver servis ayarları
+            Chrome_Servis.HideCommandPromptWindow = true; // komut satırı gizleme
+
+
+            // Parametreler
+            //Chrome_Ayar.AddArguments("--incognito"); // gizli tarayıcı
+            Chrome_Ayar.AddArgument("--disable-default-apps"); // varsayılan appleri kaldırma
+            Chrome_Ayar.AddArgument("--disable-extensions"); // varsayılan appleri kaldırma
+            Chrome_Ayar.AddArgument("--disable-gpu"); // gpu kullanımını kaldırma
+            Chrome_Ayar.AddExcludedArgument("enable-automation"); // info bar gizleme
+            Chrome_Ayar.AddAdditionalCapability("useAutomationExtension", false); // otomatik eklentiler 
+            Chrome_Ayar.AddArgument("--no-sandbox"); // emin değilim
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f";
+            string eposta = "";
+            string sifre = "";
+
+            string ytUploadUrl = "https://www.youtube.com/upload";
+
+            
+
+            Chrome_Sekme = new ChromeDriver(Chrome_Servis, Chrome_Ayar);
+            
+            // Tarayıcıyı aç
+            Chrome_Sekme.Navigate().GoToUrl(url);
+
+            // Site açılınca, gmail ile giriş yap
+            Chrome_Sekme.FindElement(By.XPath("//*[@id='openid-buttons']/button[1]")).Click();
+
+            // Eposta gir
+            Chrome_Sekme.FindElementByXPath("//input[@type='email']").SendKeys(eposta);
+            // İleri tıkla
+            Chrome_Sekme.FindElementByXPath("//*[@id='identifierNext']").Click();
+
+            Thread.Sleep(3000);
+            
+            // Şifre gir
+            Chrome_Sekme.FindElementByXPath("//input[@type='password']").SendKeys(sifre);
+            // İleri tıkla
+            Chrome_Sekme.FindElementByXPath("//*[@id='passwordNext']").Click();
+
+            Thread.Sleep(1000);
+
+            // Youtube aç
+            Chrome_Sekme.Navigate().GoToUrl(ytUploadUrl);
+            /*Chrome_Sekme.FindElementByXPath("//input[@type='file']")
+                .SendKeys(@"C:\Users\omnyvz\Desktop\TestVideo.mp4");*/
+
+
+
+        }
+
+
+
+        /// <summary>
+        /// Tarayici Kaynak Kod
+        /// </summary>
+        /// <returns></returns>
+        private string KaynakKod()
+        {
+            string DonenVeri = "";
+            Thread t_Kaynak = new Thread(delegate ()
+            {
+                try
+                {
+                    DonenVeri = Chrome_Sekme.PageSource;
+                }
+                catch
+                {
+                    DonenVeri = "false";
+                }
+            });
+            t_Kaynak.Start();
+            t_Kaynak.Join();
+            return DonenVeri;
+        }
+
+        /// <summary>
+        /// Tarayici Sonlandır
+        /// </summary>
+        /// <returns></returns>
+        public void Sonlandir()
+        {
+            try
+            {
+                Chrome_Sekme.Close();
+                Chrome_Sekme.Quit();
+                Chrome_Sekme.Dispose();
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sonlandir();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*string HTML_Kod = KaynakKod();
+            richTextBox1.Text = HTML_Kod;*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ytUploadUrl = "https://www.youtube.com/upload";
+
+            Chrome_Sekme.FindElement(By.CssSelector("body")).SendKeys(OpenQA.Selenium.Keys.Control + "t");
+            Chrome_Sekme.SwitchTo().Window(Chrome_Sekme.WindowHandles.Last());
+            Chrome_Sekme.Navigate().GoToUrl(ytUploadUrl);
+
+            /*Chrome_Sekme = new ChromeDriver(Chrome_Servis, Chrome_Ayar);
+
+            // Tarayıcıyı aç
+            Chrome_Sekme.Navigate().GoToUrl(ytUploadUrl);*/
+
+        }
+    }
+}
